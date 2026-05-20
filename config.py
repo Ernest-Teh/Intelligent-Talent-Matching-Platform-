d113abc666ef0c8b73b53c0dde4ffcf427b16265
saaadwaleedx-png <saaadwaleedx@gmail.com>
2026-05-20 18:41:50 +1000

    Final submission

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "dev-secret-key-change-later")
-
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "78cde386ba63757a1e7366c60070a43b7b75262646eba2fbf30879b8f9cdcb32")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
-
-    # ── Supabase PostgreSQL ──────────────────────────────────────────
-    # Replace YOUR_PASSWORD_HERE with your actual Supabase database password
     SQLALCHEMY_DATABASE_URI = os.environ.get(
+        
         "DATABASE_URL",
-        "postgresql://postgres.qrudvmruhboyondqwsxx:saadbinwaleed@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
+        "postgresql://postgres.qrudvmruhboyondqwsxx:saadbinwaleed@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
